535ff1b03d11b28e2809a3bfbee7d709c5a86aba
Wonder Diaz <wonderdxk@gmail.com>
2024-02-21 15:49:28 -0400

    0.0.3
    Improve to download the apk directly
    Init version currently

--- scripts.js
+++ scripts.js
@@ -7,8 +7,10 @@ async function fetchEmulators() {
         const response = await fetch('emulators.json');
         const data = await response.json();
         const emulatorsContainer = document.querySelector('.emulator-container');
-    data.forEach(emulator => {
+    data.sort((a, b) => a.name.localeCompare(b.name));
+    for (const emulator of data) {
         const emulatorItem = document.createElement('div');
+        const downloadLink = await getApkLastRelease(emulator.user, emulator.repo);
         emulatorItem.classList.add('emulator-item');
         emulatorItem.innerHTML = `
         <div class="emulator-image">
@@ -19,13 +21,39 @@ async function fetchEmulators() {
             <p class="emulator-developer">${emulator.developer}</p>
         </div>
         <div class="emulator-button">
-            <a href="${emulator.url}" target="_blank" class="install-button filled">Download</a>
+            <a href="${emulator.url.includes('https://github.com/') && downloadLink ? downloadLink : emulator.url}" target="_blank" class="install-button filled">Download</a>
         </div>`;
         emulatorsContainer.appendChild(emulatorItem);
-    });
+    }
     } catch (error) {
         console.error(error);
     }
 }
+window.onload = function() {
+    fetchEmulators();
+};
 
-fetchEmulators();
+/**
+ * The function `getApkLastRelease` fetches the latest release information from a GitHub repository and
+ * returns the download URL for the APK file if found.
+ * @param user - The `user` parameter is typically the username or organization name of the GitHub
+ * account that owns the repository you want to retrieve information from.
+ * @param repo - The `repo` parameter in the `getApkLastRelease` function represents the name of the
+ * GitHub repository from which you want to retrieve the latest release information.
+ * @returns The function `getApkLastRelease` returns the download URL of the latest APK file in the
+ * specified GitHub repository's releases. If an APK file is found, it returns the browser download URL
+ * of that APK file. If no APK file is found in the latest release, it returns the message 'No se
+ * encontró un APK en la última release.'
+ */
+const getApkLastRelease = async (user, repo) => {
+    const url = `https://api.github.com/repos/${user}/${repo}/releases/latest`;
+    try {
+        const response = await fetch(url);
+        const release = await response.json();
+        const tagName = release.tag_name;
+        const asset = release.assets.find(asset => asset.name.endsWith('.apk'));
+        return asset ? asset.browser_download_url : 'No se encontró un APK en la última release.';
+    } catch (error) {
+        console.error('Error al obtener la información:', error);
+    }
+};
